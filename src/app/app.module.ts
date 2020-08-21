a3c340a7fc97b3d1b80b1dd3f4d181051e770992
Cecilia Barasa <cecibarasa@gmail.com>
2020-08-21 12:57:20 +0300

    added profile card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { JoeComponent } from './joe/joe.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    JoeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
